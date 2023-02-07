c18445cdfc05e1f2ffc4f19c91d1cea73820e937
Yannick Schrade <yannick1218@gmail.com>
2023-02-07 22:27:09 +0100

    Fixed missing main

--- .github/workflows/scripts/createNugetPackage.ps1
+++ .github/workflows/scripts/createNugetPackage.ps1
@@ -1,7 +1,17 @@
 $solution = $args[0]
+git fetch origin main:main
 $branchName = git symbolic-ref --short HEAD
 $branchInfos = $branchName.Split('/')
 $version = $branchInfos[1].Replace('v','').Split('.')
+
+if($version.lenght -ne 3 -or  $version.lenght -ne 2){
+    Write-Error 'branch name not falid'
+}
+
+if(git rev-parse HEAD -eq 1){
+    Write-Error 'main branch does not contain last commit'
+}
+
 $commitCount = git rev-list --count --no-merges main..
 if ($branchInfos[0] -eq "release") {
     $version = "$($version[0]).$($version[1]).$($commitCount)"
